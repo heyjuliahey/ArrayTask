dc7e321024cf17dc21d297b2d2710946b253f78a
heyjuliahey <65814086+heyjuliahey@users.noreply.github.com>
2022-02-10 08:47:25 +0300

    Update CustomFileReaderTest.java

--- test/test/epam/learn/action/CustomFileReaderTest.java
+++ test/test/epam/learn/action/CustomFileReaderTest.java
@@ -1,7 +1,32 @@
 package test.epam.learn.action;
 
+import by.epam.arrayTask.exception  .CustomException;
+import by.epam.arrayTask.reader.CustomFileReader;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 class CustomFileReaderTest {
+    CustomFileReader fileReader;
+    static final String fileName = "input.txt";
 
+    @BeforeClass
+    public void setUp()
+    {
+        fileReader = new CustomFileReader();
+    }
+
+    @AfterClass
+    public void tearDown()
+    {
+        fileReader = null;
+    }
+
+    @Test
+    public void CustomFileReaderTest () throws CustomException {
+        String actual = fileReader.readFromFile(fileName);
+        String expected = "15, 28, 0, -7, 9, 31";
+        Assert.assertEquals(actual, expected);
+    }
 }
